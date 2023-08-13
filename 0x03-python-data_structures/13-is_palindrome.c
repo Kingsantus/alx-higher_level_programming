05758e2dc4455e5c1bcb1316e740a6491b21177d
kingsantus <kingsantusasogwa@gmail.com>
2023-08-13 02:37:02 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,12 +9,12 @@
 */
 int is_palindrome(listint_t **head)
 {
+	listint_t *slow = *head, *fast = *head, *prev_slow = NULL;
+	listint_t *second_half, *mid_node = NULL;
+	int isPalindrome = 1;
+
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-
-	listint_t *slow = *head, *fast = *head, *prev_slow = *head;
-	listint_t *second_half = NULL, *mid_node = NULL;
-	int isPalindrome = 1;
 
 	while (fast != NULL && fast->next != NULL)
 	{
@@ -23,13 +23,12 @@ int is_palindrome(listint_t **head)
 		slow = slow->next;
 	}
 
+	second_half = slow;
 	if (fast != NULL)
 	{
 		mid_node = slow;
 		slow = slow->next;
 	}
-
-	second_half = slow;
 	prev_slow->next = NULL;
 
 	listint_t *prev = NULL, *current = second_half, *next = NULL;
@@ -41,41 +40,23 @@ int is_palindrome(listint_t **head)
 		prev = current;
 		current = next;
 	}
-	second_half = prev;
 
 	listint_t *first_half = *head;
 
-	while (second_half != NULL)
+	while (prev != NULL)
 	{
-		if (first_half->n != second_half->n)
+		if (first_half->n != prev->n)
 		{
 			isPalindrome = 0;
 			break;
 		}
 		first_half = first_half->next;
-		second_half = second_half->next;
+		prev = prev->next;
 	}
 
-	prev = NULL;
-	current = prev_slow;
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-	prev_slow->next = prev;
-
+	prev_slow->next = second_half;
 	if (mid_node != NULL)
-	{
 		prev_slow->next = mid_node;
-		mid_node->next = second_half;
-	}
-	else
-	{
-		prev_slow->next = second_half;
-	}
 
 	return (isPalindrome);
 }
