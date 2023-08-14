647f6a7ef5374beb47f59d5c5d3cbc977aee62d6
kingsantus <kingsantusasogwa@gmail.com>
2023-08-14 14:37:02 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-* is_palindrome - Checks if a singly linked list is a palindrome
-* @head: Pointer to the head of the linked list
-* Return: 1 if palindrome, 0 otherwise
+ * is_palindrome - checks if a singly-linked list is a palindrome
+ *
+ * @head: pointer to address of first node on the list
+ * Return: 1 if list is a palindrome, otherwise 0
 */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *prev_slow = NULL;
-	listint_t *second_half, *mid_node = NULL;
-	int isPalindrome = 1;
+	listint_t *temp;
+	int list_len = 0;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (!*head)
 		return (1);
 
-	while (fast != NULL && fast->next != NULL)
+	temp = *head;
+	while (temp)
 	{
-		fast = fast->next->next;
-		prev_slow = slow;
-		slow = slow->next;
+		list_len++;
+		temp = temp->next;
 	}
+	if (list_len % 2 != 0)
+		return (0);
 
-	second_half = slow;
-	if (fast != NULL)
-	{
-		mid_node = slow;
-		slow = slow->next;
-	}
-	prev_slow->next = NULL;
-
-	listint_t *prev = NULL, *current = second_half, *next = NULL;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	listint_t *first_half = *head;
-
-	while (prev != NULL)
-	{
-		if (first_half->n != prev->n)
-		{
-			isPalindrome = 0;
-			break;
-		}
-		first_half = first_half->next;
-		prev = prev->next;
-	}
-
-	prev_slow->next = second_half;
-	if (mid_node != NULL)
-		prev_slow->next = mid_node;
-
-	return (isPalindrome);
+	return (1);
 }
