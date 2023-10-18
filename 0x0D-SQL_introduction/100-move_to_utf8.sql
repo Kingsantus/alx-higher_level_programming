784c3c0e6aac66168d8a98891d769e6f40f1cec1
kingsantus <kingsantusasogwa@gmail.com>
2023-10-17 23:15:18 -0700

    SQL - Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,8 +5,8 @@ USE hbtn_0c_0;
 
 -- Convert database to utf8mb4 character set
 ALTER DATABASE hbtn_0c_0
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 -- Convert each table in database to utf
 ALTER TABLE first_table
