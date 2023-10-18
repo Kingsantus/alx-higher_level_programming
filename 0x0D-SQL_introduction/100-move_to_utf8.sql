15ba809a03f6569f06362ee0faf29db808a012bc
kingsantus <kingsantusasogwa@gmail.com>
2023-10-17 23:22:28 -0700

    SQL - Introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,19 @@
 -- This script converts hbtn_0c_0 database to UTF8
+-- first line of code Select database
+-- second line of code Convert database to utf8mb4 character set
+-- third line of code Convert each table in database to utf
+-- fourth line of code Convert field in first_table table to UTF
 
--- Select database
 USE hbtn_0c_0;
 
--- Convert database to utf8mb4 character set
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert each table in database to utf
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Convert field in first_table table to UTF
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
