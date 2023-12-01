b1bfdc221d679a17dbd267e3667d404885cb3b29
kingsantus <kingsantusasogwa@gmail.com>
2023-12-01 10:53:59 -0800

    networking

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request and display the body of a 200 status code response
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-url=$1
-curl -s -o /dev/null -w "%{http_code}" "$url" | {
-    read -r status
-    if [ "$status" = "200" ]; then
-        curl -s "$url"
-    fi
-}
+curl -sL "$1"
