c36d6d321213c370af697a8f73f4981a9f3784e9
kingsantus <kingsantusasogwa@gmail.com>
2023-12-02 21:54:18 -0800

    networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # this script that takes in a URL, sends a request to that URL and gets a response (size in bytes)
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-url=$1
-curl -s "$url" | wc -c
+curl -s $1 | grep "Content-Length" | cut -d ' ' -f 2
