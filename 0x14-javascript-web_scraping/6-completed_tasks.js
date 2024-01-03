7c2f30786ccb1057d65d03a842995d6c3b3d1307
kingsantus <kingsantusasogwa@gmail.com>
2024-01-03 05:51:51 -0800

    fetching data from api

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+// prrinting task done by each user in an api
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const tasksData = JSON.parse(body);
+
+      if (tasksData.length > 0) {
+        const completedTasks = tasksData.filter(task => task.completed);
+
+        const completedTasksByUser = {};
+        completedTasks.forEach(task => {
+          if (completedTasksByUser[task.userId]) {
+            completedTasksByUser[task.userId]++;
+          } else {
+            completedTasksByUser[task.userId] = 1;
+          }
+        });
+
+	    console.log(JSON.stringify(completedTasksByUser, null, 2));
+      }
+    }
+  });
