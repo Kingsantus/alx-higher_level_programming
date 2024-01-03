7491d0a78a0e76642f6ea25eb98f6005713996fe
kingsantus <kingsantusasogwa@gmail.com>
2024-01-02 22:16:28 -0800

    reading and printing content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// This script reads and prints the content of a file
+
+const filepath = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(filepath, 'UTF-8', (error, content) => {
+  if (error) throw error;
+  console.log(content.toString());
+});
