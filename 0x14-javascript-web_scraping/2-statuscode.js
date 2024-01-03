0c69137d4559a63241d7ddee4821bfbeb8e565eb
kingsantus <kingsantusasogwa@gmail.com>
2024-01-02 23:25:21 -0800

    return the status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+// display the status code of a GET request
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('code:', response.statusCode);
+    }
+})
