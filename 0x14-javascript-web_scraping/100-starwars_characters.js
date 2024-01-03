7b5077e6de8f0ec1b061c06d023b8696f7b8d5f1
kingsantus <kingsantusasogwa@gmail.com>
2024-01-03 06:13:26 -0800

    printing it line by line

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// printing all character peculiar to a particular id
+
+const request = require('request');
+
+const id = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request(url, (error, resp, body) => {
+  if (error) { console.log(err); }
+  const data = JSON.parse(body);
+  for (const charURL of data.characters) {
+    request(charURL, (err, r, body) => {
+      if (err) { console.log(err); }
+      const data = JSON.parse(body);
+      console.log(data.name);
+    });
+  }
+});
