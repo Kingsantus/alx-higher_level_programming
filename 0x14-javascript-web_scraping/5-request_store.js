778f2691c0029c3ccc62c3e95268a38ead0b7fe3
kingsantus <kingsantusasogwa@gmail.com>
2024-01-03 05:27:28 -0800

    getting data from url and storing in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// getting the contents of a webpage and stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, response, body) => {
+    if (err) {
+        console.log(err);
+    } else {
+        fs.writeFile(filePath, body, 'utf-8', (writeerr) => {
+            if (writeerr) {
+                console.log(writeerr);
+            }
+        })
+    }
+})
