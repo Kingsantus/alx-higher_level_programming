e862f44152bcfffc0f17dd1f8d4ab625b1ff2c9f
kingsantus <kingsantusasogwa@gmail.com>
2024-01-02 23:02:06 -0800

    write content into a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// writing data peovided by user to file provided by used
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
