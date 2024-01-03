24a1092bbb43d5d6dcea8eda9a9f35c118f6e008
kingsantus <kingsantusasogwa@gmail.com>
2024-01-02 23:42:01 -0800

    fetching title from url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// display the status code of a GET request
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (err, response, body) => {
+    if (err) {
+        console.log(err);
+    } else {
+        const data = JSON.parse(body);
+        if (data.title) {
+            console.log(data.title);
+        } else {
+            console.log('Not found');
+        }
+    }
+})
