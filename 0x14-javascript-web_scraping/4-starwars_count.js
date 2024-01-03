c04cf3b6d7aa534802513c9216d217a99e344341
kingsantus <kingsantusasogwa@gmail.com>
2024-01-03 05:18:03 -0800

    find charcter with achiles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// This script prints the number of movies where the character “Wedge Antilles” is present
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const filmsData = JSON.parse(body);
+
+    if (filmsData.results && filmsData.results.length > 0) {
+      const moviesWithWedgeAntilles = filmsData.results.filter(movie =>
+        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+
+      console.log(moviesWithWedgeAntilles.length);
+    }
+  }
+});
